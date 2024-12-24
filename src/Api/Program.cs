c058c95fa2e3104896abd62b5e360da06252e4bf
Hiroya Yamamoto <hiroyay@microsoft.com>
2024-12-24 11:37:47 +0900

    Update Api Project

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ITelemetryInitializer, AppInsightsTelemetryInitializer>();
+
+var connectionString = builder.Configuration["SQL_CONNECTION_STRING"] ?? throw new InvalidOperationException("Connection string 'SQL_CONNECTION_STRING' not found.");
+builder.Services.AddDbContext<AdventureWorksContext>(options => options.UseSqlServer(connectionString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
--- src/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ src/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4693c6d077e07917be162088d5063f371906f94a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3144a2a2134fbd94c8adf34cbdc6c5c1679182c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
