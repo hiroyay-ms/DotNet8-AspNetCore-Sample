6179db5d0fb5641fac2bad32a248ac25a5e5c376
Hiroya Yamamoto <hiroyay@microsoft.com>
2025-02-10 14:27:15 +0900

    Update Sql Database Projects

--- src/SqlSampleDb/Script.PreDeployment1.sql
+++ src/SqlSampleDb/Script.PreDeployment1.sql
@@ -1,75 +1,55 @@
-IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('EmployeeSalesByCountry'))
-	DROP PROCEDURE [dbo].[EmployeeSalesByCountry]
-GO
-
-IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('SalesByYear'))
-	DROP PROCEDURE [dbo].[SalesByYear]
-GO
-
-IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('TenMostExpensiveProducts'))
-	DROP PROCEDURE [dbo].[TenMostExpensiveProducts]
-GO
-
-IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('SalesByCategory'))
-	DROP VIEW [dbo].[SalesByCategory]
-GO
-
-IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('SalesTotalsByAmount'))
-	DROP VIEW [dbo].[SalesTotalsByAmount]
-GO
-
-IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('ProductsByCategory'))
-	DROP VIEW [dbo].[ProductsByCategory]
-GO
-
-IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('OrderSubTotals'))
-	DROP VIEW [dbo].[OrderSubTotals]
-GO
-
-IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('OrdersQry'))
-	DROP VIEW [dbo].[OrdersQry]
-GO
-
-IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('OrderDetailsExtended'))
-	DROP VIEW [dbo].[OrderDetailsExtended]
-GO
-
-IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('Invoices'))
-	DROP VIEW [dbo].[Invoices]
-GO
-
-IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('CustomerAndSuppliersByCity'))
-	DROP VIEW [dbo].[CustomerAndSuppliersByCity]
-GO
-
 IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('OrderDetails'))
-	DROP TABLE [dbo].[OrderDetails]
-GO
+BEGIN
+	ALTER TABLE [dbo].[OrderDetails] NOCHECK CONSTRAINT ALL
+
+	DELETE FROM [dbo].[OrderDetails]
+
+	ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT ALL
+END
+
 
 IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('Orders'))
-	DROP TABLE [dbo].[Orders]
-GO
+BEGIN
+	ALTER TABLE [dbo].[Orders] NOCHECK CONSTRAINT ALL
 
-IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('Products'))
-	DROP TABLE [dbo].[Products]
-GO
+	DELETE FROM [dbo].[Orders]
 
-IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('Categories'))
-	DROP TABLE [dbo].[Categories]
+	ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT ALL
+END
 GO
 
 IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('Customers'))
-	DROP TABLE [dbo].[Customers]
-GO
-
-IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('Shippers'))
-	DROP TABLE [dbo].[Shippers]
-GO
-
-IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('Suppliers'))
-	DROP TABLE [dbo].[Suppliers]
+	DELETE FROM [dbo].[Customers]
 GO
 
 IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('Employees'))
-	DROP TABLE [dbo].[Employees]
+BEGIN
+	ALTER TABLE [dbo].[Employees] NOCHECK CONSTRAINT ALL
+
+	DELETE FROM [dbo].[Employees]
+
+	ALTER TABLE [dbo].[Employees] CHECK CONSTRAINT ALL
+END
+GO
+
+IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('Products'))
+BEGIN
+	ALTER TABLE [dbo].[Products] NOCHECK CONSTRAINT ALL
+
+	DELETE FROM [dbo].[Products]
+
+	ALTER TABLE [dbo].[Products] CHECK CONSTRAINT ALL
+END
+GO
+
+IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('Categories'))
+	DELETE FROM [dbo].[Categories]
+GO
+
+IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('Suppliers'))
+	DELETE FROM [dbo].[Suppliers]
+GO
+
+IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('Shippers'))
+	DELETE FROM [dbo].[Shippers]
 GO
