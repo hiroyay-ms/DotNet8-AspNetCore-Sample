b198e0043089476de24461e4e2648171b3969a1d
Hiroya Yamamoto <hiroyay@microsoft.com>
2025-02-14 09:29:45 +0900

    Update Function Project (itg ASP.NET Core)

--- src/FuncProjIntAspNet/Program.cs
+++ src/FuncProjIntAspNet/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
