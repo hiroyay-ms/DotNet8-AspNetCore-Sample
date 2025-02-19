a2788c38a656a1ddd4a982f2d73ab768493c75e2
Hiroya Yamamoto <hiroyay@microsoft.com>
2025-02-19 13:37:34 +0900

    Update Functions Project (itg ASP.NET Core)

--- src/AzureFunctions/FuncProjIntAspNet/Program.cs
+++ src/AzureFunctions/FuncProjIntAspNet/Program.cs
@@ -1,11 +1,22 @@
-using Microsoft.ApplicationInsights.Extensibility;
+using Azure.Monitor.OpenTelemetry.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Http;
+using OpenTelemetry.Trace;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
+    {
+        services.AddHttpContextAccessor();
+        
+        services.ConfigureOpenTelemetryTracerProvider((sp, tracerProviderBuilder) => {
+            tracerProviderBuilder.AddSource("AzureFunctions.FuncProjIntAspNet");
+            tracerProviderBuilder.AddProcessor(new LogsWithTenantIdProcessor(new HttpContextAccessor()));
+        });
+        services.AddOpenTelemetry().UseAzureMonitor();
+    })
     .Build();
 
 host.Run();
