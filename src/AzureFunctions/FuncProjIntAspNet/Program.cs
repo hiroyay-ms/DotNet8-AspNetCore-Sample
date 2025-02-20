79ea2c74909a26ed0c0e041403ce581a103a673b
Hiroya Yamamoto <hiroyay@microsoft.com>
2025-02-20 16:41:08 +0900

    Update Functions Project (Int ASP.NET Core)

--- src/AzureFunctions/FuncProjIntAspNet/Program.cs
+++ src/AzureFunctions/FuncProjIntAspNet/Program.cs
@@ -14,15 +14,13 @@ var host = new HostBuilder()
     .ConfigureServices(services =>
     {
         services.AddHttpContextAccessor();
-        
+
         services.ConfigureOpenTelemetryTracerProvider((sp, tracerProviderBuilder) => {
             tracerProviderBuilder.AddSource("AzureFunctions.FuncProjIntAspNet");
             tracerProviderBuilder.AddProcessor(new LogsWithTenantIdProcessor(new HttpContextAccessor()));
         });
         services.AddOpenTelemetry().UseAzureMonitor();
-        
-        //var connectionString = Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING") ?? throw new InvalidOperationException("Connection string 'SQL_CONNECTION_STRING' not found.");
-        //services.AddDbContext<AdventureWorksContext>(options => options.UseSqlServer(connectionString));
+
         services.AddDbContext<AdventureWorksContext>();
     })
     .Build();
